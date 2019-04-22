6d4ad844e5545f8dd130913539bd30b5eaba13a0
uwebweb <46363365+uwebweb@users.noreply.github.com>
2019-04-22 13:55:09 +0800

    Add files via upload

--- updatecore.php
+++ updatecore.php
@@ -1,11 +1,11 @@
 <?php
 // 2019-03-27
 // >= PHP 5.1.0
-// Drupal core 8.6.13
-// Drupal core 7.65
+// Drupal core 8.6.15
+// Drupal core 7.66
 
 //Setting
-$latest_drupal8_url="https://ftp.drupal.org/files/projects/drupal-8.6.13.zip";
+$latest_drupal8_url="https://ftp.drupal.org/files/projects/drupal-8.6.15.zip";
 $folders_to_copy_v8=[
   'sites/default/files',
   'core/themes',
@@ -19,7 +19,7 @@ $files_to_copy_v8=[
   //'composer.lock',
   'composer.json'
 ];
-$latest_drupal7_url="https://ftp.drupal.org/files/projects/drupal-7.65.zip";
+$latest_drupal7_url="https://ftp.drupal.org/files/projects/drupal-7.66.zip";
 $folders_to_copy_v7=[
   'sites/default/files',
   'sites/all',
